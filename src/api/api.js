dba6a4f408a7973ca91d1636947d8324acf55549
Angele <angelealst@hotmail.com>
2020-10-26 22:28:12 -0300

    #Link API produção

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // http://localhost:3000
 
 const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: "https://miniprojetoapi.herokuapp.com",
 });
 
 export default api;
